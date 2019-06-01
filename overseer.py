5cf748f9ab6f25b064b1900cf10fa03502da4549
HKBNguyen <hkbnguyen3@g.ucla.edu>
2019-05-31 17:14:50 -0700

    Added new geofence version and updates to log file.

--- overseer.py
+++ overseer.py
@@ -1,48 +1,50 @@
 import time
-import thread
-import geofence as gf
-import cutdown as cd
-import ReceiveData as rd
-
-
+import geofencev2 as gf
+#import cutdown as cd
+#import ReceiveData as rd
+import currCoords as coords
+import datetime
+import sys
 # MODIFY THESE!!! to be the location of said data within data file string
-LATITUDE = 1
-LONGITUDE = 2
-
-ERROR_VALUE = 99
 
 count=0
-check_file = open("check_coords.csv", "w+")
+check_file = open("checkCoords.csv", "w+")
+log_file = open("flightLog.txt", "w+")
 
 # Can either do 30s loop or continuous
 
 while(True):
-    all_data = open("datafilehere.csv", "r")
-    latest_data = all_data.readline()
-    latit, longit = latest_data[LATITUDE], latest_data[LONGITUDE]
-    all_data.close()
-    # latit, longit = 34.06868, -118.44331     --> dummy
-    check_file.write(str(longit), ",", str(latit), ",\n")
-    if (gf.inorout(latit, longit)):
-        count = count + 1
-        # log info this line
-    else:
-        count = 0
-    if count > 5:
-        # signal failsafe.py
-        # log this
-        cd.cut_down()
-       
-    
-    inputVal = input()
-    retVal = rd.data_receive_callback(inputVal)
-    
-    if retVal == ERROR_VALUE:
-        cd.cut_down()     
+	#all_data = open("datafilehere.csv", "r")
+	#latest_data = all_data.readline()
+	#latit, longit = latest_data[LATITUDE], latest_data[LONGITUDE]
+	#all_data.close()
+	#latit, longit = 34.06868, -118.44331
+	#latit, longit = 34.614549, -118.560853
+	latit, longit = 34.542620, -117.883676
+	#latit, longit = tm.Find_Latest_Coordinate()
+	check_file.write(str(datetime.datetime.now()) + ", " + str(latit) + ", " + str(longit) + "\n")
+	if (gf.checkSafety(latit, longit)):
+		count = count + 1
+		# log info this line
+		log_file.write(str(datetime.datetime.now()) + " Right into the Danger Zone!" + "\n")
+		#print("ERROR")
+	else:
+		count = 0
+		#print("SAFETY")
+		log_file.write(str(datetime.datetime.now()) + " In the Safety Zone!" + "\n")
+		#sleep(30)
+	if count > 20:
+		# signal failsafe.py
+		# log this
+		#cd.cut_down()
+		log_file.write(str(datetime.datetime.now()) + " Cutting down...Executing cutdown.py")
+		print("CUTDOWN")
+		sys.exit()
 
 check_file.close()
+log_file.close()
 
-    # read latest GPS coords from file -> (longit, latit)
-    # store these coords to a separate file
-    # check with geofence to see if in-out -> send result to log.py
-    # if geofence=false > 5 then signal to failsafe.py to cut -> log it
+	# read latest GPS coords from file -> (longit, latit)
+	# store these coords to a separate file
+	# check with geofence to see if in-out -> send result to log.py
+	# if geofence=false > 5 then signal to failsafe.py to cut -> log it
