fbe34d99d909a84622ec3619bd7b6ce6a118cdde
Michael Bao <michaelbao00@gmail.com>
2019-11-13 19:16:14 -0800

    updated overseer.py

--- overseer.py
+++ overseer.py
@@ -23,7 +23,7 @@ while(True):
 	latit, longit = 34.542620, -117.883676
 	#latit, longit = tm.Find_Latest_Coordinate()
 	check_file.write(str(datetime.datetime.now()) + ", " + str(latit) + ", " + str(longit) + "\n")
-	if (gf.checkSafety(latit, longit)):
+	if (gf.inDangerZone(latit, longit)):
 		count = count + 1
 		# Log info in this line
 		log_file.write(str(datetime.datetime.now()) + " Right into the Danger Zone!" + "\n")
