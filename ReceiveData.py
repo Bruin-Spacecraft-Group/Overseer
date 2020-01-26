92ef24a3a9fcecd1b8b8fb991a63be35e7ff12e8
Chenda Duan <56491285+Dadaism6@users.noreply.github.com>
2020-01-26 14:39:47 -0800

    Update ReceiveData.py

--- ReceiveData.py
+++ ReceiveData.py
@@ -30,6 +30,7 @@ def main():
         device.open()
 
         def data_receive_callback(xbee_message):
+            print("right at line 33\n")
             print("From %s >> %s" % (xbee_message.remote_device.get_64bit_addr(),
                                      xbee_message.data.decode()))
             message = xbee_message.data.decode()
