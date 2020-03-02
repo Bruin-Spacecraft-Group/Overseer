3db0207e1a4f50826d68674fb9a1ebdfb778a3a1
Michael Bao <michaelbao00@gmail.com>
2020-03-01 17:25:48 -0800

    Updated overseer.py

--- overseer.py
+++ overseer.py
@@ -1,10 +1,23 @@
 import serial
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(23, GPIO.OUT)
 
 ser = serial.Serial('/dev/ttyUSB0', 9600, timeout = .5)
 
 while True:
 	ser.write(str.encode('Hello User'))
 	incoming = ser.readline().strip()
+	print ('Received: ' + incoming)
+
 	if(incoming == 'cutdown'):
 		print('Cutting Down The Balloon')
-	print ('Received: ' + incoming)
+		GPIO.output(23, GPIO.HIGH)
+		time.sleep(3)
+		GPIO.output(23, GPIO.LOW)
+		time.sleep(3)
+		
+	
