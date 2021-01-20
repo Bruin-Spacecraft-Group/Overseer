ff60d7382a05080a3b9375a7ffa7cfb15239afb8
Ethan J Salter <66975375+ethansalter@users.noreply.github.com>
2021-01-19 16:40:05 -0800

    Update overseer.py

--- overseer.py
+++ overseer.py
@@ -1,3 +1,5 @@
+# automatic
+
 import serial
 import RPi.GPIO as GPIO
 import time
@@ -6,19 +8,10 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(23, GPIO.OUT)
 
-ser = serial.Serial('/dev/ttyUSB0', 9600, timeout = .5) 
-	# Edit for USB port used
 
 while True:
-	# Debug
-	ser.write(str.encode('Hello User'))
-
-	# Receive incoming messages
-	incoming = ser.readline().strip()
-	print ('Received: ' + incoming)
-
-	# Cuts down the balloon if the incoming message is the string 'cutdown'
-	if(incoming == 'cutdown'):
+	# Cuts down the balloon if the incoming message if conditions are met
+	if(altitude>80000 && x_pos && y_pos):
 		print('Cutting Down The Balloon')
 
 		# Cut down the balloon
