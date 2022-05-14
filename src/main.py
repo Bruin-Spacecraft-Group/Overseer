eb53e5e7afecef5ba861c321279c608041022286
Tejas Kamtam <tejaskamtam@yahoo.com>
2022-05-14 01:31:19 -0700

    async structure

--- src/main.py
+++ src/main.py
@@ -1,12 +1,23 @@
-import time
+import asyncio
+import csv
+from gpiozero import OutputDevice
 from subprocess import call
 
+# global data collection (json)
 call(["gpspipe","-w","|","fgrep","TPV",">","master.log"])
 
-while True:
-  call(["gpscsv","-n","2","-f","time,lat,lon,alt",">","output.csv"])
-  with open("output.csv", "r") as f:
-    writer = csv.writer(f)
-  
-  
-  sleep(5)
+# manual cutdown
+async def cutdown():
+  pin = OutputDevice(4)
+  pin.on()
+  asyncio.sleep(120)
+  pin.off()
+
+# data parsing (csv)
+async def data():
+  while True:
+    call(["gpscsv","-n","2","-f","time,lat,lon,alt",">","output.csv"])
+    with open("output.csv", "r") as f:
+      reader = csv.reader(f)
+      next(reader)
+
