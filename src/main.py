3a3cc458283c5dc7cc2144d50795606e081306d3
Tejas Kamtam <tejaskamtam@yahoo.com>
2022-05-14 22:06:54 -0700

    fin data parsing

--- src/main.py
+++ src/main.py
@@ -1,30 +1,46 @@
-import asyncio
+from time import sleep
 import csv
 from gpiozero import OutputDevice
 from subprocess import Popen, PIPE, STDOUT
 
-# global data collection (json) uh
-json_cmd = "gpspipe -w | fgrep TPV > master.log"
+# removed asyncio - will run global connection in seperate terminal
+
+# global data collection (json)
+#   json_cmd = "gpspipe -w | fgrep TPV > master.log"
+
+# rotating csv data
 csv_cmd = "gpscsv -n 1 -f time,lat,lon,alt > output.csv"
 
-# call(["gpspipe","-w","|","fgrep","TPV",">","master.log"])
-
-# manual cutdown
-async def cutdown():
+run = True
+# manual cutdown (endless signal)
+def cutdown():
   pin = OutputDevice(4)
   pin.on()
-  asyncio.sleep(120)
-  pin.off()
+  run = False
 
-# data parsing (csv)
-async def data():
-  while True:
+# position checking for cutdown
+def main():
+  while run:
+    # load csv data
     ps = Popen(csv_cmd, shell=True, stdout=PIPE, stderr=STDOUT)
     output = ps.communicate()[0]
+
+    # rotating data vals
+    time, lat, lon, alt = "",0.0,0.0,0.0
+
+    # csv parsing
     with open("output.csv", "r") as f:
       reader = csv.reader(f)
-      next(reader)
+      for row in reader:
+        time, lat, lon, alt = row[0], float(row[1]), float(row[2]), float(row[3])
+    
+    # pass data to geofence
+    
 
-asyncio.run(data())
+    sleep(5)
+
+
+# run
+main()
 
 
