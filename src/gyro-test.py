e4fac03e87fd6caac8d5b503b080fa24ff5709a9
Tejas Kamtam <tejaskamtam@yahoo.com>
2023-02-25 19:05:17 -0800

    test old mpu

--- src/gyro-test.py
+++ src/gyro-test.py
@@ -49,7 +49,7 @@ def read_raw_data(addr):
     return value
 
 
-bus = smbus.SMBus(1) 	# or bus = smbus.SMBus(0) for older version boards
+bus = smbus.SMBus(0) 	# or bus = smbus.SMBus(0) for older version boards
 Device_Address = 0x68   # MPU6050 device address
 
 MPU_Init()
