90f912b647dfd6f18448127776d9747f27658ec8
Tejas Kamtam <tejaskamtam@yahoo.com>
2023-02-25 19:00:16 -0800

    upload bme test

--- src/gyro-test.py
+++ src/gyro-test.py
@@ -1,3 +1,5 @@
+
+
 import mpu6050
 import smbus					#import SMBus module of I2C
 from time import sleep          #import
@@ -52,3 +54,28 @@ Device_Address = 0x68   # MPU6050 device address
 MPU_Init()
 
 print (" Reading Data of Gyroscope and Accelerometer")
+
+while True:
+	
+	#Read Accelerometer raw value
+	acc_x = read_raw_data(ACCEL_XOUT_H)
+	acc_y = read_raw_data(ACCEL_YOUT_H)
+	acc_z = read_raw_data(ACCEL_ZOUT_H)
+	
+	#Read Gyroscope raw value
+	gyro_x = read_raw_data(GYRO_XOUT_H)
+	gyro_y = read_raw_data(GYRO_YOUT_H)
+	gyro_z = read_raw_data(GYRO_ZOUT_H)
+	
+	#Full scale range +/- 250 degree/C as per sensitivity scale factor
+	Ax = acc_x/16384.0
+	Ay = acc_y/16384.0
+	Az = acc_z/16384.0
+	
+	Gx = gyro_x/131.0
+	Gy = gyro_y/131.0
+	Gz = gyro_z/131.0
+	
+
+	print ("Gx=%.2f" %Gx, u'\u00b0'+ "/s", "\tGy=%.2f" %Gy, u'\u00b0'+ "/s", "\tGz=%.2f" %Gz, u'\u00b0'+ "/s", "\tAx=%.2f g" %Ax, "\tAy=%.2f g" %Ay, "\tAz=%.2f g" %Az) 	
+	sleep(1)
