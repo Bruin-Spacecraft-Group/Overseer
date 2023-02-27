26fe6ea53622af8c87341d34cb8116b11c01fc85
Huy Nguyen <28833768+Haderachi@users.noreply.github.com>
2023-02-27 12:07:27 -0800

    Update gps-test.py

--- src/gps-test.py
+++ src/gps-test.py
@@ -1,7 +1,6 @@
 import json
 import os
 import sys
-from gpsdata_functions import *
 
 def main():
 	convert_nmea_to_json()
@@ -77,5 +76,60 @@ def convert_nmea_to_json():
 		json.dump(gpsdata_json, f, ensure_ascii=False, indent = 4)
 		f.close()
 	
+# Helper functions for printing various kinds of GPS data
+
+# Prints all GPS data
+def get_all(file):
+	with open(file, "r") as file:
+		data = json.load(file)
+		for item in data:
+			for key in item:
+				print(f'{key}: ', item[key])
+			print("\n")
+	sys.exit(0)
+
+# Prints only longitude and latitude, with directions
+def get_lon_lat(file):
+	with open(file, "r") as file:
+		data = json.load(file)
+		for item in data:
+			print("Longitude: ", item["Longitude"])
+			print("Longitude Direction: ", item["Longitude Direction"])
+			print("Latitude: ", item["Latitude"])
+			print("Latitude Direction: ", item["Latitude Direction"])
+			print("\n")
+	sys.exit(0)
+		
+# Prints altiude
+def get_alt(file):
+	with open(file, "r") as file:
+		data = json.load(file)
+		for item in data:
+			print("Altitude: ", item['Altitude'])
+			print("Altitude Unit: ", item['Altitude Unit'])
+			print("\n")
+	sys.exit(0)
+
+# Prints geoid altitude
+def get_geoid_alt(file):
+	data = json.load(file)
+	for item in data:
+		print("Geoid Altitude: ", item['geoidAlt'])
+		print("Geoid Altitude Unit: ", item['geoidAlt Unit'])
+		print("\n")
+	sys.exit(0)
 	
+# Prints fix quality
+def get_fix_qual(file):
+	data = json.load(file)
+	for item in data:
+		if item['fixQual'] == 1:
+			print("Fix quality: GPS")
+		elif item['fixQual'] == 2:
+			print("Fix Quality: DGPS")
+		else:
+			print("Invalid fix quality")
+		print("\n")
+	sys.exit(0)
+
 main()
