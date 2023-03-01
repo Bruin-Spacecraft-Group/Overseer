2861469a87b2eb2c33a8dff64ea042bc5b60ee7f
Tejas Kamtam <tejaskamtam@yahoo.com>
2023-03-01 12:55:17 -0800

    temp test 1

--- src/mpu6050/mpu6050.py
+++ src/mpu6050/mpu6050.py
@@ -7,7 +7,7 @@ mpu = adafruit_mpu6050.MPU6050(i2c)
 
 temp_offset = 0
 
-print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (mpu.acceleration + temp_offset))
+print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (mpu.acceleration))
 print("Gyro X:%.2f, Y: %.2f, Z: %.2f degrees/s" % (mpu.gyro))
-print("Temperature: %.2f C" % mpu.temperature)
+print("Temperature: %.2f C" % (mpu.temperature + temp_offset))
 time.sleep(1)
