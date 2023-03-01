6ba72681aa7f3a30a15d8b68007293eca174a3bd
Tejas Kamtam <tejaskamtam@yahoo.com>
2023-03-01 11:48:06 -0800

    temp offset

--- src/bme680/bme680-test.py
+++ src/bme680/bme680-test.py
@@ -2,12 +2,14 @@ import adafruit_bme680
 import time
 import board
 
+# on 0x77
+
 # Create sensor object, communicating over the board's default I2C bus
-i2c = board.I2C()   # uses board.SCL and board.SDA
+i2c = board.I2C()   # defaults 0x77
 bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c)
 
 # change this to match the location's pressure (hPa) at sea level
-bme680.sea_level_pressure = 1013.25
+# bme680.sea_level_pressure = 1013.25
 
 temperature = bme680.temperature
 gas = bme680.gas
@@ -15,8 +17,10 @@ relative_humidity = bme680.relative_humidity
 pressure = bme680.pressure
 altitude = bme680.altitude
 
+temp_offset = -7.0
+
 while True:
-    print("\nTemperature: %0.1f C" % bme680.temperature)
+    print("\nTemperature: %0.1f C" % bme680.temperature + temp_offset)
     print("Gas: %d ohm" % bme680.gas)
     print("Humidity: %0.1f %%" % bme680.relative_humidity)
     print("Pressure: %0.3f hPa" % bme680.pressure)
