cf254162de0515ea16ffd9a6898647b998d94b3a
Tejas Kamtam <tejaskamtam@yahoo.com>
2023-03-01 11:55:02 -0800

    revert prints

--- src/bme680/bme680-test.py
+++ src/bme680/bme680-test.py
@@ -9,7 +9,7 @@ i2c = board.I2C()   # defaults 0x77
 bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c)
 
 # change this to match the location's pressure (hPa) at sea level
-# bme680.sea_level_pressure = 1013.25
+bme680.sea_level_pressure = 1013.25
 
 temperature = bme680.temperature
 gas = bme680.gas
@@ -17,13 +17,12 @@ relative_humidity = bme680.relative_humidity
 pressure = bme680.pressure
 altitude = bme680.altitude
 
-temp_offset = -7.0
+temp_offset = -7
 
-while True:
-    print("Temperature:", bme680.temperature + temp_offset, " \0C")
-    print("Gas:", bme680.gas, "ohm")
-    print("Relative Humidity:", bme680.relative_humidity)
-    print("Pressure:", bme680.pressure, "hPa")
-    print("Altitude:", bme680.altitude, "m")
+print("\nTemperature: %0.1f C" % bme680.temperature + temp_offset)
+print("Gas: %d ohm" % bme680.gas)
+print("Humidity: %0.1f %%" % bme680.relative_humidity)
+print("Pressure: %0.3f hPa" % bme680.pressure)
+print("Altitude = %0.2f meters" % bme680.altitude)
 
-    time.sleep(2)
+time.sleep(2)
