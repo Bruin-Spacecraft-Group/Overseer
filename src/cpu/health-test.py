12fdf189256c0ec06a87baec02d679645c5128ea
Sonav Agarwal <42980809+SonavAgarwal@users.noreply.github.com>
2023-03-09 18:07:41 -0800

    Update health-test.py

--- src/cpu/health-test.py
+++ src/cpu/health-test.py
@@ -18,33 +18,20 @@ cpuUsers = [x.decode() for x in cpuUsers]
 cpuUsage = mpstatLines[3].split()
 cpuUsage = [x.decode() for x in cpuUsage]
 
-
-
 usageObject = {}
 
 usageObject["usr"] = cpuUsage[cpuUsers.index("%nice")]
 usageObject["sys"] = cpuUsage[cpuUsers.index("%sys")]
 usageObject["idle"] = cpuUsage[cpuUsers.index("%idle")]
 
-
-# for i in range(3, min(7, len(cpuUsers))):
-#     usageObject[str(cpuUsers[i][1:].decode())] = str(cpuUsage[i])
-
 usageObjectJson = json.dumps(usageObject)
-
-print(usageObjectJson)
-
-print(cpuUsers)
-print(cpuUsage)
-
-
 
 #create json object
 outputObject = {
     "temp": str(cpu.temperature),
     "c": str(clockOutput),
     "v": str(voltsOutput),
-    "top": str(usageObjectJson),
+    "cpu": str(usageObjectJson),
 }
 
 # output json object
