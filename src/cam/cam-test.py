1e1b45703a0cba4184ef16a8524714e448d6e3a4
srikarnimma <nsrikar18@gmail.com>
2023-03-09 17:35:02 -0800

    record if reaches 200 feet

--- src/cam/cam-test.py
+++ src/cam/cam-test.py
@@ -1,17 +1,24 @@
 from picamera import PiCamera
 from datetime import datetime
 
+# sets up camera
 camera = PiCamera()
 
-# record 5 seconds
-# camera.start_preview()
-# camera.start_recording('video.h264')
-# camera.wait_recording(5)
-# camera.stop_recording()
-# camera.stop_preview()
+#intializes (TODO altitude should be given)
+alt = 0
+reachedTreshold = False
 
-# take a picture
-fname = datetime.now().strftime("%H-%M-%S") + ".jpg"
-camera.start_preview()
-camera.capture(fname)
-camera.stop_preview()
+#checks if we reached 200 feet, and if video not alr taken
+if alt >= 200 and not reachedTreshold:
+    #records and saves video
+    fname = datetime.now().strftime("%H-%M-%S") + ".h264"
+    camera.start_preview()
+    camera.start_recording(fname)
+    camera.wait_recording(25)
+    camera.stop_recording()
+    camera.stop_preview()
+
+    #so we don't try recording again
+    reachedTreshold = True
+
+
