5ec2e11a2a04ec2abcbb4b2b5fecbcfdb721598a
Alex <alexbranchpb@gmail.com>
2023-03-10 01:47:14 -0800

    Parses output of gpspipe -w -n 5 and saves to a dictionary

--- src/gps/gps_data.py
+++ src/gps/gps_data.py
@@ -1,174 +1,43 @@
 import json
 import subprocess
-import sys
+
+'''
+Runs gpspipe which outputs json objects.
+Parses json and saves data we want to a dictionary
+
+Notes:
+Number of runs=5 because in testing we found this is when the data first comes in.
+Might need to change this based on the output after the first 5 runs
+'''
 
 def gps_data():
-    # subprocess.run(["gpspipe", "-w", "-n", "5"])
-    f = open("gpsdata.json", "r")
+
+    # TODO: Test on pi to ensure file creation and piping works properly.
+
+    f = open("gps_data.json", "w") # create file
+    subprocess.run(["gpspipe", "-w", "-n", "5"], stdout=f) # run and pipe to file
+    f = open("gps_data.json", "r") # read file
+    gps_data = dict()
     for line in f:
         json_loaded = json.loads(line)
-        if json_loaded["class"] == "TPV":
-            parse_json(json_loaded)
+        if json_loaded["class"] == "TPV": # only get TPV object
+            gps_data = parse_json(json_loaded)
+            return gps_data
+
+    print("No objects found")
+    return gps_data
           
 
 def parse_json(json_data):
-    keywords = ["lat", "lon", "altHAE", "epx", "epy", "epv", "speed", "climb", "eps", "epc"]
+    keywords = ["lat", "lon", "altHAE", "epx", "epy", "epv", "speed", "climb", "eps", "epc"] # keywords we want
+    data_dict = dict()
     for keyword in keywords:
-       print(f'{keyword}: ' + str(json_data[keyword]))
+        data_dict[keyword] = json_data[keyword] # save data we want to a dictionary
+    return data_dict
+
 
 def main():
-    gps_data()
+    print(gps_data())
 
 if __name__ == "__main__":
     main()
-
-
-
-'''
-import json
-import os
-import sys
-
-# Name of output files
-GPSDATA_FNAME = "gpsdata.json"
-NMEA_RAW_FNAME = "nmea_raw.txt"
-
-
-def main():
-    convert_nmea_to_json()
-
-    print("Press 1 to display all data.")
-    print("Press 2 to display longitude and latitude.")
-    print("Press 3 to display altitude.")
-    print("Press 4 to display geoid altitude")
-    print("Press \"q\" to quit.")
-
-    # Available helper functions
-    commands = {
-        1: get_all,
-        2: get_lon_lat,
-        3: get_alt,
-        4: get_geoid_alt,
-        "q": sys.exit
-    }
-
-    command = input(">>> ")
-    if command.isnumeric():
-        try:
-            commands[int(command)](GPSDATA_FNAME)
-        except FileNotFoundError:
-            print("File is inaccessible.")
-            sys.exit(1)
-    else:
-        try:
-            commands[command]()
-        except KeyError:
-            print("No such command available.")
-            sys.exit(1)
-
-
-# Captures GPS NMEA sentences, convert them into JSON objects and output to JSON file
-def convert_nmea_to_json():
-    # Idea: Build a big list of JSON objects and dump all into a JSON file
-    gpsdata_json = []
-
-    # Captures 100 GPS NMEA sentences
-    os.system("gpspipe -w -n 10 > nmea_raw.txt")
-
-    with open(NMEA_RAW_FNAME, "r") as file:
-        for line in file:
-            GPS_DATA = {
-                'fixTime': None,
-                'Latitude': None,
-                'Latitude Direction': None,
-                'Longitude': None,
-                'Longitude Direction': None,
-                'fixQual': None,
-                'Number of Satellites': None,
-                'HDOP': None,
-                'Altitude': None,
-                'Altitude Unit': None,
-                'geoidAlt': None,
-                'geoidAlt Unit': None,
-            }
-            sentence = line.split(",")
-
-            # The NMEA sentence we need
-            if ((sentence[0] == "$GPGGA" or sentence[0] == "$GNGGA")):
-                # Build up JSON object containing GPS data
-                for i, k in enumerate(['fixTime', 'Latitude', 'Latitude Direction', 'Longitude', 'Longitude Direction', 'fixQual', 'Number of Satellites', 'HDOP', 'Altitude', 'Altitude Unit', 'geoidAlt', 'geoidAlt Unit']):
-                    GPS_DATA[k] = sentence[i+1]
-                gpsdata_json.append(GPS_DATA)
-
-        # Writes JSON object list to file
-        f = open(GPSDATA_FNAME, "w")
-        json.dump(gpsdata_json, f, ensure_ascii=False, indent=4)
-        f.close()
-
-# Helper functions for displaying various kinds of GPS data
-
-# Prints all GPS data
-
-
-def get_all(file):
-    with open(file, "r") as file:
-        data = json.load(file)
-        for item in data:
-            for key in item:
-                print(f'{key}: ', item[key])
-            print("\n")
-    sys.exit(0)
-
-
-# Prints only longitude and latitude, with directions
-def get_lon_lat(file):
-    with open(file, "r") as file:
-        data = json.load(file)
-        for item in data:
-            print("Longitude: ", item["Longitude"])
-            print("Longitude Direction: ", item["Longitude Direction"])
-            print("Latitude: ", item["Latitude"])
-            print("Latitude Direction: ", item["Latitude Direction"])
-            print("\n")
-    sys.exit(0)
-
-
-# Prints altiude
-def get_alt(file):
-    with open(file, "r") as file:
-        data = json.load(file)
-        for item in data:
-            print("Altitude: ", item['Altitude'])
-            print("Altitude Unit: ", item['Altitude Unit'])
-            print("\n")
-    sys.exit(0)
-
-# Prints geoid altitude
-
-
-def get_geoid_alt(file):
-    data = json.load(file)
-    for item in data:
-        print("Geoid Altitude: ", item['geoidAlt'])
-        print("Geoid Altitude Unit: ", item['geoidAlt Unit'])
-        print("\n")
-    sys.exit(0)
-
-
-# Prints fix quality
-def get_fix_qual(file):
-    data = json.load(file)
-    for item in data:
-        if item['fixQual'] == 1:
-            print("Fix quality: GPS")
-        elif item['fixQual'] == 2:
-            print("Fix Quality: DGPS")
-        else:
-            print("Invalid fix quality")
-        print("\n")
-    sys.exit(0)
-
-
-main()
-
-''' 
