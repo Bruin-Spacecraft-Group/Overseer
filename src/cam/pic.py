18b4bdc8fd906372ce4f639df3a210470de7b462
Tejas Kamtam <tejaskamtam@yahoo.com>
2023-03-09 19:14:28 -0800

    pic testing

--- src/cam/pic.py
+++ src/cam/pic.py
@@ -1,16 +1,24 @@
 # take a pic using the camera and save it to the current folder
 
-import picamera
-import time
+from datetime import datetime
+from picamera import PiCamera
+
+# def take_pic():
+#     with PiCamera() as camera:
+#         camera.resolution = (1024, 768)
+#         camera.start_preview()
+#         time.sleep(2)
+#         name
+#         camera.capture('image.jpg')
 
 
-def take_pic():
-    with picamera.PiCamera() as camera:
-        camera.resolution = (1024, 768)
-        camera.start_preview()
-        time.sleep(2)
-        camera.capture('image.jpg')
+# if __name__ == '__main__':
+#     take_pic()
 
 
-if __name__ == '__main__':
-    take_pic()
+camera = PiCamera()
+# take a picture
+fname = datetime.now().strftime("%H-%M-%S") + ".jpg"
+camera.start_preview()
+camera.capture(fname)
+camera.stop_preview()
