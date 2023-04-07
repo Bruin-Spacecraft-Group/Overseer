896b0e75238b82359ed82028edf2a74e2900879b
Sonav Agarwal <42980809+SonavAgarwal@users.noreply.github.com>
2023-04-06 18:33:57 -0700

    new shorter output in main

--- src/main.py
+++ src/main.py
@@ -10,28 +10,38 @@ from picamera import PiCamera
 from gpiozero import CPUTemperature
 import json
 import subprocess
+from decimal import Decimal
 
 # 1. CPU Health - print temp, clock, volt, top
 
-
 def get_cpu_temp():
-    # get cpu temperature
     cpu = CPUTemperature()
-    print("CPU Temperature: " + str(cpu.temperature))
 
-    clockOutput = subprocess.check_output(
-        ['vcgencmd', 'measure_clock', 'arm'])  # clock
-    voltsOutput = subprocess.check_output(
-        ['vcgencmd', 'measure_volts', 'core'])  # cpu voltage
-    topOutput = subprocess.check_output(['mpstat'])  # cpu usage
+    clockOutput = subprocess.check_output(['vcgencmd', 'measure_clock', 'arm']).decode()[:-1] # clock
+    clockOutput = clockOutput[clockOutput.index("=") + 1:]
+    voltsOutput = subprocess.check_output(['vcgencmd', 'measure_volts', 'core']).decode()[:-1] # cpu voltage
+    voltsOutput = voltsOutput[voltsOutput.index("=") + 1:]
+    mpstatOutput = subprocess.check_output(['mpstat']) # cpu usage
 
-    # create json object
+    mpstatLines = mpstatOutput.splitlines()
+    cpuUsers = mpstatLines[2].split()
+    cpuUsers = [x.decode() for x in cpuUsers]
+    cpuUsage = mpstatLines[3].split()
+    cpuUsage = [x.decode() for x in cpuUsage]
+
+    usageObject = {}
+    usageObject["idle"] = cpuUsage[cpuUsers.index("%idle")]
+
+    usageObjectJson = json.dumps(usageObject)
+
+    #create json object
     outputObject = {
-        "temp": str(cpu.temperature),
+        "t": str(cpu.temperature),
         "c": str(clockOutput),
         "v": str(voltsOutput),
-        "top": str(topOutput),
+        "cpu": str(usageObjectJson),
     }
+
     # output json object
     outputJSON = json.dumps(outputObject)
     print(outputJSON)
