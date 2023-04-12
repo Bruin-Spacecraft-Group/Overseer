1572f8d5759b066ae522989272b723f869106e23
Tejas Kamtam <tejaskamtam@yahoo.com>
2023-04-11 20:18:04 -0700

    organizing + bme280

--- src/main.py
+++ src/main.py
@@ -85,7 +85,30 @@ def mpu6050():
     print("Gyro X:%.2f, Y: %.2f, Z: %.2f degrees/s" % (mpu.gyro))
     print("Temperature (MPU): %.2f C" % (mpu.temperature + temp_offset))
 
+# 4. BME280 - print temp, pressure, humidity
+def bme280():
+    # Create sensor object, communicating over the board's default I2C bus
+    i2c = board.I2C() # defualts 0x77
+    bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c)
 
+    # change this to match the location's pressure (hPa) at sea level
+    bme680.sea_level_pressure = 1014.22
+
+    temperature = bme680.temperature
+    gas = bme680.gas
+    relative_humidity = bme680.relative_humidity
+    pressure = bme680.pressure
+    altitude = bme680.altitude
+
+    temp_offset = -6.5
+    print("Temperature (BME): %0.1f C" % (bme680.temperature + temp_offset))
+    print("Gas Resistance: %d ohm" % bme680.gas)
+    print("Relative Humidity: %0.1f %%" % bme680.relative_humidity)
+    print("Pressure: %0.3f hPa" % bme680.pressure)
+    print("Altitude = %0.2f meters" % bme680.altitude)
+
+# time.sleep(2)
 get_cpu_temp()
 camera()
 mpu6050()
+bme280()
