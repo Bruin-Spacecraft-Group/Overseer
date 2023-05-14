55949301c9d251c3b9bd5ea2af5328177335b6a9
Sonav Agarwal <42980809+SonavAgarwal@users.noreply.github.com>
2023-05-14 16:00:31 -0700

    Update uplink.py

--- src/uplink.py
+++ src/uplink.py
@@ -46,12 +46,12 @@ def checkOutputFile():
     if "cutdown" in lastTenLinesString:
         print("Activating Cutdown")
 
-        try:
-            with open("flight_log.txt", "a+") as f:
-                f.write(cutdown())
-            f.close()
-        except:
-            print("Cutdown Error")
+        # try:
+        #     with open("flight_log.txt", "a+") as f:
+        #         f.write(cutdown())
+        #     f.close()
+        # except:
+        #     print("Cutdown Error")
 
     if "picture" in lastTenLinesString:
         print("Taking Picture")
