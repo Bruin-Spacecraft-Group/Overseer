74aaac6b2404c77e61314308b9bdc3d29c92f4bb
Sonav Agarwal <42980809+SonavAgarwal@users.noreply.github.com>
2023-05-14 14:43:18 -0700

    Update StartBalloon.sh

--- src/StartBalloon.sh
+++ src/StartBalloon.sh
@@ -1,3 +1,4 @@
 
 # direwolf start, output to console and file, start uplink script
-direwolf -c ~/direwolf/flight_mode.conf | tee ~/FLIGHT_DATA_S23/flight_output.txt & python3 ~/Overseer/src/uplink.py
+# TODO: MAKE TEST
+direwolf -c ~/direwolf/test_mode.conf | tee ~/FLIGHT_DATA_S23/flight_output.txt & python3 ~/Overseer/src/uplink.py
