a8fa242d6e25f5cafaa651448cab0fcc16e03c1b
Tejas Kamtam <tejaskamtam@yahoo.com>
2023-05-18 20:08:58 -0700

    listify

--- src/gps_v2.py
+++ src/gps_v2.py
@@ -7,15 +7,12 @@ def gps():
   with open("gps_data.json", "w") as f:
     subprocess.run(["gpspipe", "-w", "-n", "5"], stdout=f)
   with open("gps_data.json", "r") as f:
-    gps_data = dict()
+    gps_data = list()
     for line in f:
       json_loaded = json.loads(line)
-      print(json_loaded)
       if json_loaded["class"] == "TPV":
-        gps_data = json_loaded["class"=="TPV"]
+        gps_data = list(json_loaded["lat"], json_loaded["lon"], json_loaded["altHAE"], json_loaded["epx"], json_loaded["epy"], json_loaded["epv"], json_loaded["speed"], json_loaded["climb"], json_loaded["eps"], json_loaded["epc"])
         return gps_data
-    print("No objects found")
-    return gps_data
 
 
 
