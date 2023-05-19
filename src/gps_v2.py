1de1dab7ef57daafd46404851119ef9d0e72ab95
Tejas Kamtam <tejaskamtam@yahoo.com>
2023-05-18 20:06:32 -0700

    test

--- src/gps_v2.py
+++ src/gps_v2.py
@@ -1,29 +1,22 @@
 
 import json, subprocess
 
-def parse_json(json_data):
-  # caapture values of keys ["lat", "lon", "altHAE", "epx", "epy", "epv", "speed", "climb", "eps", "epc"]
-  keywords = ["lat", "lon", "epx", "epy", "epv", "speed", "climb", "eps", "epc"]
-  data_dict = dict()
-  for keyword in keywords:
-    data_dict[keyword] = json_data[keyword]
-  return data_dict
-
-
+# capture values of keys ["lat", "lon", "altHAE", "epx", "epy", "epv", "speed", "climb", "eps", "epc"]
 
 def gps():
-    with open("gps_data.json", "w") as f:
-        subprocess.run(["gpspipe", "-w", "-n", "5"], stdout=f)
-    with open("gps_data.json", "r") as f:
-        gps_data = dict()
-        for line in f:
-            json_loaded = json.loads(line)
-            if json_loaded["class"] == "TPV":
-                print(json_loaded)
-                gps_data = parse_json(json_loaded)
-                return gps_data
-        print("No objects found")
+  with open("gps_data.json", "w") as f:
+    subprocess.run(["gpspipe", "-w", "-n", "5"], stdout=f)
+  with open("gps_data.json", "r") as f:
+    gps_data = dict()
+    for line in f:
+      json_loaded = json.loads(line)
+      print(json_loaded)
+      if json_loaded["class"] == "TPV":
+        gps_data = json_loaded["class"=="TPV"]
         return gps_data
+    print("No objects found")
+    return gps_data
+
 
 
 print(gps())
