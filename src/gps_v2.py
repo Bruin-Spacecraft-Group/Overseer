4b64bb526b3b6c7da181f59541fb4a420a7e1b1c
Tejas Kamtam <tejaskamtam@yahoo.com>
2023-05-19 18:10:37 -0700

    try again

--- src/gps_v2.py
+++ src/gps_v2.py
@@ -4,7 +4,8 @@ import json, subprocess
 # capture values of keys ["lat", "lon", "altHAE", "epx", "epy", "epv", "speed", "climb", "eps", "epc"]
 
 def gps():
-  rets = list(2)
+  rets = []
+  json_out = {}
   with open("gps_data.json", "w") as f:
     subprocess.run(["gpspipe", "-w", "-n", "5"], stdout=f)
   with open("gps_data.json", "r") as f:
@@ -12,8 +13,49 @@ def gps():
     for line in f:
       json_loaded = json.loads(line)
       if json_loaded["class"] == "TPV":
-        rets[0] = json_loaded
+        json_data = json_loaded
         break
+  json_out = rets[0]
+  try:
+    rets.append(json_out["lat"])
+  except:
+    rets.append("e")
+  try:
+    rets.append(json_out["lon"])
+  except:
+    rets.append("e")
+  try:
+    rets.append(json_out["altHAE"])
+  except:
+    rets.append("e")
+  try:
+    rets.append(json_out["epx"])
+  except:
+    rets.append("e")
+  try:
+    rets.append(json_out["epy"])
+  except:
+    rets.append("e")
+  try:
+    rets.append(json_out["epv"])
+  except:
+    rets.append("e")
+  try:
+    rets.append(json_out["speed"])
+  except:
+    rets.append("e")
+  try:
+    rets.append(json_out["climb"])
+  except:
+    rets.append("e")
+  try:
+    rets.append(json_out["eps"])
+  except:
+    rets.append("e")
+  try:
+    rets.append(json_out["epc"])
+  except:
+    rets.append("e")
   return rets
 
 
