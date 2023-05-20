4de370f6962ff62389dc31e28bf932edaa9af25e
Tejas Kamtam <tejaskamtam@yahoo.com>
2023-05-19 18:13:34 -0700

    try wo json and minimiize imports

--- src/gps_v2.py
+++ src/gps_v2.py
@@ -1,5 +1,5 @@
 
-import json, subprocess
+from subprocess import run
 
 # capture values of keys ["lat", "lon", "altHAE", "epx", "epy", "epv", "speed", "climb", "eps", "epc"]
 
@@ -7,7 +7,7 @@ def gps():
   rets = []
   json_out = {}
   with open("gps_data.json", "w") as f:
-    subprocess.run(["gpspipe", "-w", "-n", "5"], stdout=f)
+    run(["gpspipe", "-w", "-n", "5"], stdout=f)
   with open("gps_data.json", "r") as f:
     gps_data = []
     for line in f:
