8fb997a3a88ab5123ac1e929dd414dcdeffe8cb7
Sonav Agarwal <42980809+SonavAgarwal@users.noreply.github.com>
2023-05-20 09:22:59 -0700

    fix cutdown error message

--- src/uplink.py
+++ src/uplink.py
@@ -2,35 +2,10 @@
 import time
 import os
 
-from picamera import PiCamera
-from datetime import datetime
-
 from gpiozero import LED
 GPIO_PIN = 27
 
-# Camera - take a picture
-
-# WE DONT USE THIS ANYMORE - because we can only have 1 instance at a time and race condition is bad with main
-def camera():
-    camera = PiCamera()
-    camera.resolution = (1920, 1080)
-
-    cwd = os.getcwd()
-    os.chdir("/home/overseer/FLIGHT_DATA_S23/PICTURES")
-    fname = datetime.now().strftime("%H:%M:%S") + ".h264"
-
-    camera.start_preview()
-    camera.start_recording(fname)
-    camera.wait_recording(5)
-    camera.stop_recording()
-    camera.stop_preview()
-
-    os.chdir(cwd)
-    
-    return fname
-
 # TODO: Cutdown function w/ nichrome test
-
 
 def cutdown():
     print("Cutdown activated")
@@ -38,47 +13,31 @@ def cutdown():
     pin.on()
     time.sleep(4)
     pin.off()
-    sleep(2)
+    time.sleep(2)
     pin.on()
-    sleep(4)
+    time.sleep(4)
     pin.off()
 
 
 monitor_file_path = "~/FLIGHT_DATA_S23/flight_output.txt"
-
 
 def checkOutputFile():
     file = open(os.path.expanduser(monitor_file_path), "r")
     fileContent = file.readlines()
     file.close()
 
-    lastTenLines = fileContent[-10:]
-    lastTenLinesString = "\n".join(lastTenLines)
+    lastNLines = fileContent[-5:]
+    lastNLinesString = "\n".join(lastNLines)
 
     # IMPORTANT: choose string
     searchString = "cutdownOverseer"
 
-    if searchString in lastTenLinesString:
+    if searchString in lastNLinesString:
         print("Activating Cutdown")
-
         try:
-            with open("flight_log.txt", "a+") as subfile:
-                subfile.write(cutdown())
-            subfile.close()
+            cutdown()
         except:
-            print("Cutdown Error")
-
-    searchString = "activateVideo"
-
-    if searchString in lastTenLinesString:
-        print("Taking Video")
-
-        try:
-            with open("flight_log.txt", "a+") as subfile:
-                subfile.write(camera())
-            subfile.close()
-        except:
-            print("Video Error")
+            print("Error activating cutdown")
 
 
 while True:
