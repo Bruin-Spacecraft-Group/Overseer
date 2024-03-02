9232dd967627a6fcdbd9a7bec49c540c517dcf49
Lucas Schweighofer <lucas_s_29@icloud.com>
2024-03-01 16:18:44 -0800

    practice

--- training/Dataloading/wildfire_extraction.py
+++ training/Dataloading/wildfire_extraction.py
@@ -4,23 +4,28 @@ import math
 def read_coordinates_from_csv(file_path):
     coordinates = []
     names = []
-
-    #TODO: open the csv file, read it using csv.DictReader, add coordinates (lat,long) and names to arrays
-    #       - remember we want to get fires within a one degree radius of Tahoe (39.5624° N, 120.5635° W) <-- how can we do this?
-
+    with open(file_path, 'r', encoding='utf8') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            latitude = float(row['Latitude'])
+            longitude = float(row['Longitude'])
+            name = str(row['Name'])
+            distance = math.sqrt((latitude - 39.5624)**2 + (longitude + 120.5635)**2)
+            if distance <= 1:
+                coordinates.append((latitude, longitude))
+                names.append(name)
     return coordinates, names
 
 def write_coordinates_to_txt(coordinates, names, output_file):
-    #TODO: create a file named <output_file>, write data into file (hint you can still use the open() function!, do it line by line)
-
-    #see example_output.txt to see formatting (look into python f strings)
-
-    return
+    with open(output_file, 'w') as file:
+        for i, (lat, lon) in enumerate(coordinates, start=1):
+            file.write(f"Number {i}. {names[i-1]}: Latitude {lat}, Longitude {lon}\n")
+            file.close
+    return coordinates, names
 
 # Example usage
 file_path = 'California_Fire_Incidents.csv'
-output_file = 'output_coordinates.txt'
-
+output_file = 'OutputFile.txt'
 coordinates, names = read_coordinates_from_csv(file_path)
 
 if coordinates is not None:
