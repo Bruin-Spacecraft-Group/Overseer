ccf21c2e0b102d75f3e699d9697db968bc4136ca
srikarnimma <nsrikar18@gmail.com>
2024-05-17 17:32:44 -0700

    added pictures to cam

--- src/cam/cam_test.py
+++ src/cam/cam_test.py
@@ -7,11 +7,16 @@ class MyCamera:
         self.camera.resolution = (1920, 1080)
 
     def record_video(self, vid_fname, pic_fname, altitude, reached_threshold):
+        #takes image
+        self.camera.start_preview()
+        self.camera.capture(pic_fname)
+        self.camera.stop_preview()
+
         #checks if we reached 100 feet and if video not already taken
         if altitude >= 100 and not reached_threshold:
             #records and saves video
             self.camera.start_preview()
-            self.camera.start_recording(fname)
+            self.camera.start_recording(vid_fname)
             self.camera.wait_recording(10)
             self.camera.stop_recording()
             self.camera.stop_preview()
